04b9ad4b8b13582b1a76d4c52f18c7d52789090d
seongyooo <104564818+seongyooo@users.noreply.github.com>
2025-06-10 17:01:53 +0900

    Update rwlock.c 3step time stamp

--- 1_courses/operating-systems/assignments/rwlock/rwlock.c
+++ 1_courses/operating-systems/assignments/rwlock/rwlock.c
@@ -5,34 +5,28 @@
 #include <string.h>
 #include <unistd.h> // usleep() 사용하기 위함
 #include <pthread.h>
+#include <sys/time.h>
 
 #define rw_count 100
 #define BUFFER_SIZE 100
 
-// void create_sequence_file();
-// void rwlock_init(rwlock_t *rw);
-// void rwlock_acquire_readlock(rwlock_t *rw);
-// void rwlock_release_readlock(rwlock_t *rw);
-// void rwlock_acquire_writelock(rwlock_t *rw);
-// void rwlock_release_writelock(rwlock_t *rw);
 void *reader(void *arg);
 void *writer(void *arg);
 
-// typedef struct _rwlock_t{
-//     sem_t lock;
-//     sem_t write_lock;
-//     int readers;
-// } rwlock_t;
 
 typedef struct{
     int id;
     int processing_time;
+    struct timeval program_start_time; //time stamp 하기 위함 -> [0.0000]
 
 } thread_args;
 
 
 
 int main(int argc, char *argv[]){
+
+    struct timeval my_time;
+    gettimeofday(&my_time, NULL);
 
     if(argc !=2){
         printf("Usage: ./rwlock <sequence file>\n");
@@ -45,42 +39,44 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    char thread[rw_count];
+    char thread_type[rw_count];
     char rw_type[10];
     int processing_time=0;
-    int read_count=0, write_count=0, total_count=-1;
+    int read_count=0, write_count=0, total_count=-1; // read, write 따로 증가하기에 변수 구별
 
     pthread_t threads[rw_count];
 
-    while(fgets(thread, BUFFER_SIZE, fp) != NULL){
+    while(fgets(thread_type, BUFFER_SIZE, fp) != NULL){ // fgets로 line을 읽어서 나중에 공백으로 sccanf로 변수들을 구분분
         thread_args *ta = malloc(sizeof(thread_args));
+        ta->program_start_time = my_time;
+
         if(ta == NULL){
             perror("Error malloc NULL");
             continue;
         }
 
-        if(sscanf(thread, "%s %d", rw_type, &processing_time) !=2){
+        if(sscanf(thread_type, "%s %d", rw_type, &processing_time) !=2){
             perror("Error not two condition");
             continue;
         };
         
         total_count++;
 
-        if(!strcmp(rw_type, "R")){ 
+        if(!strcmp(rw_type, "R")){ // rw_type이 R이면, reader 스레드 실행
             ta->id = ++read_count;
             ta->processing_time = processing_time;
 
-            if(pthread_create(&thread[total_count], NULL, reader, (void*)ta) != 0){
+            if(pthread_create(&threads[total_count], NULL, reader, (void*)ta) != 0){
                 perror("Error: failed create read thread");
                 free(ta);
                 total_count--;
             }
         }
-        else if(!strcmp(rw_type, "W")){
+        else if(!strcmp(rw_type, "W")){ // rw_type이 W이면, writer 스레드 실행
             ta->id = ++write_count;
             ta->processing_time = processing_time;
 
-            if(pthread_create(&thread[total_count], NULL, writer, (void*)ta) !=0){
+            if(pthread_create(&threads[total_count], NULL, writer, (void*)ta) !=0){
                 perror("Error: failed create write thread");
                 free(ta);
                 total_count--;
@@ -91,7 +87,7 @@ int main(int argc, char *argv[]){
     }
 
     for(int i=0; i<=total_count; i++){
-        pthread_join(thread[i], NULL);
+        pthread_join(threads[i], NULL);
     }
 
     fclose(fp);
@@ -103,74 +99,31 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+typedef struct time_stamp{
+    long second;
+    long ms;
+}time_stamp;
 
-// // 100개의 read/write 작업 + 100~500의 processing time을 갖는 작업을 생성
-// void create_sequence_file() {
-//     FILE *file = fopen("sequence.txt", "w");
-//     if (file == NULL) {
-//         printf("Error opening file\n");
-//         return;
-//     }
+time_stamp time_stamp_cal(struct timeval *cur_time){
 
-//     char rw[2] = {'R', 'W'};
-//     int random_number = 0;
-
-//     for (int i = 0; i < rw_count; i++) {
-//         random_number = (rand() % 5 + 1) * 100;
-//         int rw_random = rand() % 2;
-
-//         fprintf(file, "%c %d\n", rw[rw_random], random_number);
-//     }
-
-//     fclose(file);
-// }
-
-// void read_sequence_file(rwlock_t *rw){
-//     FILE *file = fopen("sequence.txt", "r");
-//     if (file == NULL) {
-//         printf("Error opening file\n");
-//         return;
-//     }
-// }
-
-
-// void rwlock_init(rwlock_t *rw){
-//     rw->readers = 0;
-//     sem_init(&rw->lock, 0, 1);
-//     sem_init(&rw->write_lock, 0, 1);
-// }
-
-// void rwlock_acquire_readlock(rwlock_t *rw){
-//     sem_wait(&rw->lock);
-//     rw->readers++;
-//     if(rw->readers == 1){
-//         sem_wait(&rw->write_lock);
-//     }
-//     sem_post(&rw->lock);
-// }
-
-// void rwlock_release_readlock(rwlock_t *rw){
-//     sem_wait(&rw->lock);
-//     rw->readers--;
-//     if(rw->readers == 0){
-//         sem_post(&rw->write_lock);
-//     }
-//     sem_post(&rw->lock);
-// }
-
-// void rwlock_acquire_writelock(rwlock_t *rw){
-//     sem_wait(&rw->write_lock);
-// }
-
-// void rwlock_release_writelock(rwlock_t *rw){
-//     sem_post(&rw->write_lock);
-// }
+}
 
 void *reader(void *arg){
     thread_args *ta = (thread_args*)arg;
 
-    printf("[] Reader#%d: Created!\n", ta->id); 
-    printf("[] Reader#%d: Read started! (reading %d ms)\n", ta->id, ta->processing_time);
+    struct timeval cur_time;
+    gettimeofday(&cur_time, NULL); //프린트하기 전에 이것을 다시 계산할 필요가 있음.(정확한 시간 결과를 얻을려면)
+    long second, ms;
+    second = cur_time.tv_sec - ta->program_start_time.tv_sec;
+    ms = cur_time.tv_usec - ta->program_start_time.tv_usec;
+
+    if(ms<0){ // usec 0~999,999 이므로 10000000이 sec이므로 그만큼 더해줌줌
+        second--;
+        ms+=1000000;
+    }
+
+    printf("[%ld.%04ld] Reader#%d: Created!\n", second, ms/100,  ta->id); // usec가 6자리이기에 네자리 고정으로 할려면 100으로 나눠서 4자리로 만들면 된다다
+    printf("[%ld.%04ld] Reader#%d: Read started! (reading %d ms)\n", second, ms/100, ta->id, ta->processing_time);
 
     usleep(ta->processing_time * 1000);
     printf("[] Reader#%d: Terminated\n", ta->id); 
