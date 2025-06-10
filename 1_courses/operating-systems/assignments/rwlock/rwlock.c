3513d55b33357a39674b02439d8f6a7b1867cb53
seongyooo <104564818+seongyooo@users.noreply.github.com>
2025-06-10 19:09:45 +0900

    Update rwlock.c addtion timp_stamp_cal()

--- 1_courses/operating-systems/assignments/rwlock/rwlock.c
+++ 1_courses/operating-systems/assignments/rwlock/rwlock.c
@@ -7,7 +7,6 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#define rw_count 100
 #define BUFFER_SIZE 100
 
 void *reader(void *arg);
@@ -39,14 +38,15 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    char thread_type[rw_count];
+    char line_buffer[BUFFER_SIZE];
     char rw_type[10];
+
     int processing_time=0;
     int read_count=0, write_count=0, total_count=-1; // read, write 따로 증가하기에 변수 구별
 
-    pthread_t threads[rw_count];
+    pthread_t threads[BUFFER_SIZE];
 
-    while(fgets(thread_type, BUFFER_SIZE, fp) != NULL){ // fgets로 line을 읽어서 나중에 공백으로 sccanf로 변수들을 구분분
+    while(fgets(line_buffer, BUFFER_SIZE, fp) != NULL){ // fgets로 line을 읽어서 나중에 공백으로 sccanf로 변수들을 구분분
         thread_args *ta = malloc(sizeof(thread_args));
         ta->program_start_time = my_time;
 
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]){
             continue;
         }
 
-        if(sscanf(thread_type, "%s %d", rw_type, &processing_time) !=2){
+        if(sscanf(line_buffer, "%s %d", rw_type, processing_time) !=2){
             perror("Error not two condition");
             continue;
         };
@@ -104,29 +104,39 @@ typedef struct time_stamp{
     long ms;
 }time_stamp;
 
-time_stamp time_stamp_cal(struct timeval *cur_time){
+time_stamp time_stamp_cal(struct timeval start_time){
+    time_stamp result;
 
+    struct timeval cur_time;
+    gettimeofday(&cur_time, NULL);
+
+    result.second = cur_time.tv_sec - start_time.tv_sec;
+    result.ms = cur_time.tv_usec - start_time.tv_usec;
+
+    if (result.ms < 0) { 
+        result.second--;
+        result.ms += 1000000; 
+    }
+
+    return result; 
 }
 
 void *reader(void *arg){
     thread_args *ta = (thread_args*)arg;
+    time_stamp ts;
 
-    struct timeval cur_time;
-    gettimeofday(&cur_time, NULL); //프린트하기 전에 이것을 다시 계산할 필요가 있음.(정확한 시간 결과를 얻을려면)
-    long second, ms;
-    second = cur_time.tv_sec - ta->program_start_time.tv_sec;
-    ms = cur_time.tv_usec - ta->program_start_time.tv_usec;
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Reader#%d: Created!\n", ts.second, ts.ms/100,  ta->id); // usec가 6자리이기에 네자리 고정으로 할려면 100으로 나눠서 4자리로 만들면 된다
 
-    if(ms<0){ // usec 0~999,999 이므로 10000000이 sec이므로 그만큼 더해줌줌
-        second--;
-        ms+=1000000;
-    }
-
-    printf("[%ld.%04ld] Reader#%d: Created!\n", second, ms/100,  ta->id); // usec가 6자리이기에 네자리 고정으로 할려면 100으로 나눠서 4자리로 만들면 된다다
-    printf("[%ld.%04ld] Reader#%d: Read started! (reading %d ms)\n", second, ms/100, ta->id, ta->processing_time);
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Reader#%d: Read started! (reading %d ms)\n", ts.second, ts.ms/100, ta->id, ta->processing_time);
 
     usleep(ta->processing_time * 1000);
-    printf("[] Reader#%d: Terminated\n", ta->id); 
+
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Reader#%d: Terminated\n",ts.second, ts.ms/100, ta->id); 
+
+
     free(ta); 
     return NULL;
 
@@ -134,12 +144,19 @@ void *reader(void *arg){
 
 void *writer(void *arg){
     thread_args *ta = (thread_args*)arg;
+    time_stamp ts;
 
-    printf("[] Writer#%d: Created!\n", ta->id); 
-    printf("[] Writer#%d: Write started! (writing %d ms)\n", ta->id, ta->processing_time);
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Writer#%d: Created!\n", ts.second, ts.ms/100, ta->id); 
+
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Writer#%d: Write started! (writing %d ms)\n",ts.second, ts.ms/100, ta->id, ta->processing_time);
+
 
     usleep(ta->processing_time * 1000);
-    printf("[] Writer#%d: Terminated\n", ta->id); 
+
+    ts = time_stamp_cal(ta->program_start_time);
+    printf("[%ld.%04ld] Writer#%d: Terminated\n",ts.second, ts.ms/100, ta->id); 
     free(ta); 
     return NULL;
  
